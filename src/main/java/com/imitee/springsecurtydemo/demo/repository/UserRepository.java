d77bfc4ce6369864cb2372d9cb1691defd615dfa
Luo <2192807680@qq.com>
2021-02-20 18:37:35 +0800

    init

--- src/main/java/com/imitee/springsecurtydemo/demo/repository/UserRepository.java
+++ src/main/java/com/imitee/springsecurtydemo/demo/repository/UserRepository.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Repository;
 public class UserRepository {
 
     public User findByUsername(String username) {
+        // 这里模拟从数据库中获取用户
+        // TODO 请自己修改定义数据库查询
         return new User("lwj", "123", AuthorityUtils.commaSeparatedStringToAuthorityList("admin") );
     }
 }
